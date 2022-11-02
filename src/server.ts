6e5628b1119ea58dc6c75d7e9ea22d3a9da96097
Karina Borges <karina_borges@id.uff.br>
2022-11-02 11:02:14 -0300

    ✨ Creation of DB with prisma

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+import cors from "@fastify/cors";
 import Fastify from "fastify";
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   });
 
-  fastify.get("/pools/count", () => {
-    return { count: 0 };
+  await fastify.register(cors, {
+    //for production define origin
+    origin: true,
   });
 
-  await fastify.listen({ port: 3333 });
+  fastify.get("/pools/count", async () => {
+    const count = await prisma.pool.count();
+
+    return { count };
+  });
+
+  await fastify.listen({ port: 3333, host: "0.0.0.0" });
 }
 
 bootstrap();
