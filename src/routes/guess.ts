74bc78018f722bdef39b6fd4eaaf8e91011ce374
Karina Borges <karina_borges@id.uff.br>
2022-11-06 15:49:33 -0300

    ✨ create services Auth and Pools

---
+++ src/routes/guess.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../lib/prisma";
+
+export const guessRoutes = async (fastify: FastifyInstance) => {
+  fastify.get("/guesses/count", async () => {
+    const count = await prisma.guess.count();
+
+    return { count };
+  });
+};
